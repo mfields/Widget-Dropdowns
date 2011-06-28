468ad361dc61c9f8b266346fac807e82b78018ac
Michael Fields <michael@mfields.org>
2011-06-28 11:24:32 -0700

    Second???

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Register Custom Widget Area.
+ *
+ * This function currently has a pretty ugly hack that
+ * should be dealt with before publicly-release. The
+ * "after_title" argument opens a div with a class attr
+ * of "dropdown" which is then closed by the "after_widget"
+ * argument. This is totally hack-tacular and was done
+ * because I am lazy and just wanted to see this thing work.
+ *
+ * Patches welcome!
+ */
+function poc_widget_dropdowns_widgets_init() {
+	register_sidebar( array(
+		'name'          => 'Dropdowns',
+		'id'            => 'dropdowns',
+		'description'   => 'Dropdowns that appear at the top of the page on all views.',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div></div>',
+		'before_title'  => '<h3 class="trigger">',
+		'after_title'   => '</h3><div class="dropdown">',
+	) );
+}
+
+add_action( 'widgets_init', 'poc_widget_dropdowns_widgets_init' );
---
+++ readme.md
@@ -0,0 +1,13 @@
+Widget Dropdowns
+================
+
+This is a simple proof-of-concept piece which demonstrates the use of a widget area as a "mega menu" dropdown. It is packaged as a WordPress theme for easy installation and portability. Please see [http://wordpress.mfields.org/2011/dropdown-widgets/].
+
+Installation
+------------
+
+1. Download the most recent version.
+2. Add it to wp-content/themes.
+3. Log into WordPress.
+4. Activate the Widget Dropdowns theme.
+5. Add a few widgets to the "Dropdowns" widget area.
